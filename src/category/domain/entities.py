0705184438369e3e2034942478f20dc869915d59
Thyago Alves <thyagoasantiago@gmail.com>
2023-02-09 22:28:57 -0400

    Aula 11

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,9 +1,18 @@
 from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
 
+@dataclass()
 class Category:
+  
+  name:str
+  description: Optional[str] = None
+  is_active:Optional[bool] = True
+  created_at:Optional[datetime] = field(default_factory=lambda: datetime.now())
 
-  def __init__(self, name:str, description:str, is_active:bool, created_at:datetime) -> None:
-    self.name = name
-    self.description = description
-    self.is_active = is_active
-    self.created_at = created_at
+
+
+print(Category('Movie2'))
+print(Category('Movie2'))
+print(Category('Movie2'))
+print(Category('Movie2'))
