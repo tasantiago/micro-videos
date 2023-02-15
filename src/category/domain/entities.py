76989e12a6cdb27527a326732a78a9824dd9cdcd
Thyago Alves <thyagoasantiago@gmail.com>
2023-02-14 20:57:09 -0400

    Aula 25

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from dataclasses import dataclass, field
 from typing import Optional
+import uuid
 
-@dataclass()
+@dataclass(kw_only=True)
 class Category:
-  
+
+  id: uuid.UUID() = field(default_factory=lambda: uuid.uuid4())  
   name:str
   description: Optional[str] = None
   is_active:Optional[bool] = True
